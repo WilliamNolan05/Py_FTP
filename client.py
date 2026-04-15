96e4bea675fe6a7f152c112cbb21bc98d167f76f
20107135 <20107153@mail.wit.ie>
2026-04-15 17:25:58 +0100

    created classes for Auth

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket 
+from prompt_toolkit import prompt
+import threading 
 
 s = socket.socket()
 
@@ -9,7 +11,19 @@ print(str(s.recv(1024).decode()))
 
 
 class Auth: 
-    def login():
+    def PASS():
+          s.send("PASS".encode())
+          
+          
+          
+    def USER():
             s.send("USER".encode())
+            if str(s.recv(1024).decode()) == "331":
+                  username = (prompt("Username: ")).encode()
+                  s.send(username)
+                  if str(s.recv(1024).decode()) == 'Username Correct':  
+                    print("Username was correct")
+                    Auth.PASS()
+            return    
 
-Auth.login()
+Auth.USER()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket 
+import threading
 
 port = 12399
 
@@ -9,11 +10,26 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((addr,port))
 s.listen(5)
 
+auth_user = 'tony'
+auth_password = 'gabagool'
+
+class Auth:
+    def USER():
+        print("User Command Was Selected")
+        status_code = ("331").encode()
+        client.send(status_code)
+
 while True:
     client, addr = s.accept()
-    msg = ("Thank you for connecting").encode()
+    msg = ("Thank you for connecting! ").encode()
     client.send(msg)
     if str(client.recv(1024).decode()) == "USER":
+        Auth.USER()
         print("User Command Was Selected")
-        status_code =("200").encode()
+        status_code = ("331").encode()
         client.send(status_code)
+        username = client.recv(1024).decode()
+        if username == auth_user:
+            client.send(('Username Correct').encode())
+        else:
+            client.send(('Incorrect Username').encode())
